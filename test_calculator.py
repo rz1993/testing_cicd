fd27d82df0d28bb077e8b0b463aac7ad12c662b9
rzhou1993 <rzhou4@binghamton.edu>
2019-01-06 19:36:37 -0500

    First Commit

---
+++ test_calculator.py
@@ -0,0 +1,11 @@
+import calculator
+
+class CalculatorTest:
+    def test_add(self):
+        assert 4 == calculator.add(2, 2)
+
+    def test_subtract(self):
+        assert 2 == calculator.subtract(4, 2)
+
+    def test_divide(self):
+        assert 2 == calculator.divide(6, 3)
